ed6b042cf35e9a481314faf6774ab6cbb961bc52
retarf <retarf@gmail.com>
2018-01-18 15:00:23 +0100

    snake.move() - don't let to move through self

--- snake.py
+++ snake.py
@@ -1,16 +1,23 @@
+#!/bin/env python
+
 from curses import wrapper
 import curses
 
 
 class Snake:
 
-    def __init__(self, screen, pos_y, pos_x, length):
+    def __init__(self, screen, pos_y=-1, pos_x=-1, length=3, speed=100):
         self.screen = screen
+        (self.screen_y, self.screen_x) = screen.getmaxyx()
         self.x = pos_x
+        if self.x == -1:
+            self.x = int(self.screen_x/2)
         self.y = pos_y
+        if self.y == -1:
+            self.y = int(self.screen_y/2)
         self.length = length
-        self.speed = 50  # speed in ms
-        self.direction = self.move_up
+        self.speed = speed  # speed in ms
+        self.direction = self.move_right
         self.look = "@"
         self.body = []
         self.body.append((self.y, self.x))
@@ -61,25 +68,29 @@ class Snake:
         '''Move snake after sec second, takes keys to change direction'''
         c = self.screen.getch()
         curses.flushinp()
+        print(self.direction)
 
-        if c == curses.KEY_UP:
+        if c == curses.KEY_UP and self.direction != self.move_down:
             self.move_up()
             self.direction = self.move_up
 
-        elif c == curses.KEY_DOWN:
+        elif c == curses.KEY_DOWN and self.direction != self.move_up:
             self.move_down()
             self.direction = self.move_down
 
-        elif c == curses.KEY_RIGHT:
+        elif c == curses.KEY_RIGHT and self.direction != self.move_left:
             self.move_right()
             self.direction = self.move_right
 
-        elif c == curses.KEY_LEFT:
+        elif c == curses.KEY_LEFT and self.direction != self.move_right:
             self.move_left()
             self.direction = self.move_left
 
-        elif c == -1:
+        else:
             self.direction()
+
+    def eat(self):
+        pass
 
 
 
@@ -87,7 +98,7 @@ def main(screen):
     screen.clear()
     screen.nodelay(True)
     curses.curs_set(False)
-    snake = Snake(screen, 25, 84, 30)
+    snake = Snake(screen)
     while True:
         snake.show()
         snake.move()
